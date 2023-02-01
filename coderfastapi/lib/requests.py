bbea765358fdf485d6f6c068fe55f954f40e9c23
Robin Siep <RobinSiep1@gmail.com>
2023-02-01 16:17:57 +0100

    Delete empty constructor

--- coderfastapi/lib/requests.py
+++ coderfastapi/lib/requests.py
@@ -7,9 +7,6 @@ from fastapi import Request
 class RequestWithSession(Request):
     user_id: Optional[UUID] = None
 
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-
     @staticmethod
     def from_request(request: Request) -> Self:
         return RequestWithSession(
