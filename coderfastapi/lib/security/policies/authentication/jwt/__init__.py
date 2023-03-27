e2c5050b16be1e58775ef8919dc6b4d68217969b
Robin Siep <RobinSiep1@gmail.com>
2023-03-27 16:06:26 +0200

    Seperated create_access_token

--- coderfastapi/lib/security/policies/authentication/jwt/__init__.py
+++ coderfastapi/lib/security/policies/authentication/jwt/__init__.py
@@ -80,11 +80,10 @@ class JWTAuthenticationPolicy(AuthenticationPolicy):
         data = {}
         for provider in self.providers:
             data.update(provider.parse_to_encode(kwargs))
-        return jwt.encode(
-            data,
-            self.secret_key,
-            algorithm=self.algorithm,
-        )
+        return self._create_token(self, data)
+
+    def _create_token(self, data: dict[str, Any]) -> str:
+        return jwt.encode(data, self.secret_key, algorithm=self.algorithm)
 
 
 def get_auth_method_and_token(
