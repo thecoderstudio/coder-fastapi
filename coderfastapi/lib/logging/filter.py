8183fab03e4685d5e8d086b1e9be69ab95700d5c
Robin Siep <RobinSiep1@gmail.com>
2023-09-27 11:45:00 +0200

    Cleaned up code a bit

--- coderfastapi/lib/logging/filter.py
+++ coderfastapi/lib/logging/filter.py
@@ -1,3 +1,4 @@
+import copy
 import logging
 
 from google.cloud.logging_v2.handlers import (
@@ -13,8 +14,18 @@ class CloudLoggingFilter(GoogleCloudLoggingFilter):
 
         trace_id, span_id = cloud_trace_context.get()
         if trace_id:
-            record.trace = f"projects/{self.project}/traces/{trace_id}"
-            record.span_id = span_id
+            record = self._add_trace_data(record, trace_id, span_id)
 
         super().filter(record)
         return True
+
+    def _add_trace_data(
+        self,
+        record: logging.LogRecord,
+        trace_id: str,
+        span_id: str,
+    ) -> logging.LogRecord:
+        record = copy.copy(record)
+        record.trace = f"projects/{self.project}/traces/{trace_id}"
+        record.span_id = span_id
+        return record
