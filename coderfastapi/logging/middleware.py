de016e74ddefeb4eb42e1be051545354cbe8023c
Robin Siep <RobinSiep1@gmail.com>
2023-09-29 12:12:31 +0200

    Rewrite logging middleware to pure ASGI middleware

--- coderfastapi/logging/middleware.py
+++ coderfastapi/logging/middleware.py
@@ -1,10 +1,8 @@
-import logging
+from collections.abc import Callable
 from contextvars import ContextVar
-from http import HTTPStatus
 
-from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
-from starlette.responses import JSONResponse, Response
+from starlette.types import ASGIApp
 
 from coderfastapi.lib.validation.schemas.request import HTTPRequestSchema
 from coderfastapi.logging.context import CloudTraceContext
@@ -13,34 +11,31 @@ TRACE_HEADER = "x-cloud-trace-context"
 INTERNAL_SERVER_ERROR = "Internal Server Error"
 
 
-class LoggingMiddleware(BaseHTTPMiddleware):
+class LoggingMiddleware:
+    app: ASGIApp
     cloud_trace_context: CloudTraceContext
     http_request_context: ContextVar
 
     def __init__(
         self,
+        app: ASGIApp,
         cloud_trace_context: CloudTraceContext,
         http_request_context: ContextVar,
         *args,
         **kwargs,
     ) -> None:
-        super().__init__(*args, **kwargs)
+        self.app = app
         self.cloud_trace_context = cloud_trace_context
         self.http_request_context = http_request_context
 
-    async def dispatch(
-        self, request: Request, call_next: RequestResponseEndpoint
-    ) -> Response:
+    async def __call__(self, scope: dict, receive: Callable, send: Callable) -> None:
+        if scope["type"] != "http":
+            return await self.app(scope, receive, send)
+
+        request = Request(scope, receive=receive)
         if TRACE_HEADER in request.headers:
             self.cloud_trace_context.set(request.headers[TRACE_HEADER])
 
         self.http_request_context.set(HTTPRequestSchema.from_request(request))
 
-        try:
-            return await call_next(request)
-        except Exception:
-            logging.exception(INTERNAL_SERVER_ERROR)
-            return JSONResponse(
-                status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
-                content=INTERNAL_SERVER_ERROR,
-            )
+        await self.app(scope, receive, send)
--- tests/logging/test_middleware.py
+++ tests/logging/test_middleware.py
@@ -3,54 +3,88 @@ from unittest.mock import patch
 from coderfastapi.logging.middleware import TRACE_HEADER, LoggingMiddleware
 
 
-async def test_logging_middleware_dispatch(mocker):
+async def test_logging_middleware_call(mocker):
     trace_header_val = "TRACE HEADER"
-    app_mock = mocker.MagicMock()
+    scope = {"type": "http"}
+    app_mock = mocker.AsyncMock()
+    receive_mock = mocker.MagicMock()
+    send_mock = mocker.MagicMock()
     trace_context_mock = mocker.MagicMock()
     request_context_mock = mocker.MagicMock()
     request_mock = mocker.MagicMock(headers={TRACE_HEADER: trace_header_val})
-    call_next_mock = mocker.AsyncMock()
     logging_middleware = LoggingMiddleware(
-        trace_context_mock,
-        request_context_mock,
         app=app_mock,
+        cloud_trace_context=trace_context_mock,
+        http_request_context=request_context_mock,
     )
 
-    with patch(
-        "coderfastapi.logging.middleware.HTTPRequestSchema.from_request"
-    ) as schema_from_request_mock:
-        await logging_middleware.dispatch(request_mock, call_next_mock)
+    with (
+        patch(
+            "coderfastapi.logging.middleware.HTTPRequestSchema.from_request"
+        ) as schema_from_request_mock,
+        patch(
+            "coderfastapi.logging.middleware.Request", return_value=request_mock
+        ) as request_init_mock,
+    ):
+        await logging_middleware.__call__(scope, receive_mock, send_mock)
 
     trace_context_mock.set.assert_called_once_with(trace_header_val)
     schema_from_request_mock.assert_called_once_with(request_mock)
     request_context_mock.set.assert_called_once_with(
         schema_from_request_mock(request_mock)
     )
+    request_init_mock.assert_called_once_with(scope, receive=receive_mock)
+    app_mock.assert_awaited_once_with(scope, receive_mock, send_mock)
 
-    call_next_mock.assert_awaited_once_with(request_mock)
 
-
-async def test_logging_middleware_dispatch_without_cloud_trace(mocker):
-    app_mock = mocker.MagicMock()
+async def test_logging_middleware_call_without_cloud_trace(mocker):
+    scope = {"type": "http"}
+    app_mock = mocker.AsyncMock()
+    receive_mock = mocker.MagicMock()
+    send_mock = mocker.MagicMock()
     trace_context_mock = mocker.MagicMock()
     request_context_mock = mocker.MagicMock()
     request_mock = mocker.MagicMock(headers={})
-    call_next_mock = mocker.AsyncMock()
     logging_middleware = LoggingMiddleware(
-        trace_context_mock,
-        request_context_mock,
         app=app_mock,
+        cloud_trace_context=trace_context_mock,
+        http_request_context=request_context_mock,
     )
 
-    with patch(
-        "coderfastapi.logging.middleware.HTTPRequestSchema.from_request"
-    ) as schema_from_request_mock:
-        await logging_middleware.dispatch(request_mock, call_next_mock)
+    with (
+        patch(
+            "coderfastapi.logging.middleware.HTTPRequestSchema.from_request"
+        ) as schema_from_request_mock,
+        patch(
+            "coderfastapi.logging.middleware.Request", return_value=request_mock
+        ) as request_init_mock,
+    ):
+        await logging_middleware.__call__(scope, receive_mock, send_mock)
 
     trace_context_mock.set.assert_not_called()
     schema_from_request_mock.assert_called_once_with(request_mock)
     request_context_mock.set.assert_called_once_with(
         schema_from_request_mock(request_mock)
     )
+    request_init_mock.assert_called_once_with(scope, receive=receive_mock)
+    app_mock.assert_awaited_once_with(scope, receive_mock, send_mock)
 
-    call_next_mock.assert_awaited_once_with(request_mock)
+
+async def test_logging_middleware_call_not_http_request(mocker):
+    scope = {"type": "websocket"}
+    app_mock = mocker.AsyncMock()
+    receive_mock = mocker.MagicMock()
+    send_mock = mocker.MagicMock()
+    trace_context_mock = mocker.MagicMock()
+    request_context_mock = mocker.MagicMock()
+    logging_middleware = LoggingMiddleware(
+        app=app_mock,
+        cloud_trace_context=trace_context_mock,
+        http_request_context=request_context_mock,
+    )
+
+    await logging_middleware.__call__(scope, receive_mock, send_mock)
+
+    trace_context_mock.set.assert_not_called()
+    request_context_mock.set.assert_not_called()
+    app_mock.assert_awaited_once_with(scope, receive_mock, send_mock)
